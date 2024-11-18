1bfbaf0b9ac5ed0b6e5e6df49bc19b716467e391
Neo Yin <yinzhang@berkeley.edu>
2024-11-17 17:47:58 -0800

    allowing pretiled h5

--- scripts/test_one_slide.py
+++ scripts/test_one_slide.py
@@ -2,10 +2,10 @@ import os
 from LLBMA.front_end.api import analyse_bma
 
 slide_path = (
-    "/media/hdd3/neo/error_slides_ndpi/H19-6490;S10;MSKM - 2023-12-11 21.02.14.ndpi"
+    "/media/hdd3/neo/error_slides_ndpi/H24-3456;S20;MSK9 - 2024-05-20 12.14.45.ndpi"
 )
 pretiled_h5_path = (
-    "/media/hdd3/neo/error_slides_h5/H19-6490;S10;MSKM - 2023-12-11 21.02.14.h5"
+    "/media/hdd3/neo/error_slides_h5/H24-3456;S20;MSK9 - 2024-05-20 12.14.45.h5"
 )
 dump_dir = "/media/hdd3/neo/test_error_results_dir"
 
