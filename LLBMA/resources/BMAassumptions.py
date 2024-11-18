ae2b0ddd6576b68fca0ab29d07e1025f1bae5862
Neo Yin <yinzhang@berkeley.edu>
2024-11-18 09:17:57 -0800

    runtime profiling for multislide testing script

--- LLBMA/resources/BMAassumptions.py
+++ LLBMA/resources/BMAassumptions.py
@@ -78,7 +78,7 @@ allowed_reading_time = 60  # in seconds
 region_cropping_batch_size = 512
 region_saving_batch_size = 512
 region_clf_batch_size = 32
-high_mag_region_clf_batch_size = 20
+high_mag_region_clf_batch_size = 16
 cell_clf_batch_size = 256
 YOLO_batch_size = 32
 
