3172ed86cfb61ff79fa6fb35f4afe0542f5797d4
Neo Yin <yinzhang@berkeley.edu>
2024-12-03 13:34:01 -0800

    get slide tiles for Brenda

--- scripts/tile_slide_for_brenda.py
+++ scripts/tile_slide_for_brenda.py
@@ -1,4 +1,5 @@
 import os
+import time
 import shutil
 
 slide_name = "H18-9786;S10;MSKM - 2023-06-21 21.41.10"
@@ -10,5 +11,9 @@ tmp_save_dir = "/media/hdd3/neo/brenda_tmp"
 
 os.makedirs(tmp_save_dir, exist_ok=True)
 
+starttime = time.time()
+print(f"Copying slide {slide_name} to {tmp_save_dir}")
 # rsync the slide to the tmp_save_dir
 shutil.copy(slide_path, tmp_save_dir)
+print(f"Slide {slide_name} copied to {tmp_save_dir}")
+print(f"Time taken: {time.time() - starttime} seconds")
