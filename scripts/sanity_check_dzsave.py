5b27d786168811f918857411023bee1b8b44fab6
Neo Yin <yinzhang@berkeley.edu>
2024-12-11 10:55:49 -0800

    sanity check dzsave

---
+++ scripts/sanity_check_dzsave.py
@@ -0,0 +1,12 @@
+import h5py
+from LLBMA.tiling.dzsave_h5 import retrieve_tile_h5
+
+dzsave_h5_path = "/media/hdd3/neo/test_error_results_dir_tmp/test_slide/slide.h5"
+
+# open the h5 file
+with h5py.File(dzsave_h5_path, "r") as f:
+    # get the "0" dataset
+    dataset = f["0"]
+
+    # print the the [0,0] element of the dataset
+    print(dataset[0, 0])
