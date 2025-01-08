5538ccc0b41831025e2ada3cf8d2f09a8f0fe1b9
Neo Yin <yinzhang@berkeley.edu>
2025-01-08 00:19:10 -0800

    decreasing batch size as temporary OOM fix

--- LLBMA/resources/BMAassumptions.py
+++ LLBMA/resources/BMAassumptions.py
@@ -81,10 +81,10 @@ allowed_reading_time = 60  # in seconds
 
 region_cropping_batch_size = 512
 region_saving_batch_size = 512
-region_clf_batch_size = 32
-high_mag_region_clf_batch_size = 10
-cell_clf_batch_size = 256
-YOLO_batch_size = 32
+region_clf_batch_size = 16
+high_mag_region_clf_batch_size = 8
+cell_clf_batch_size = 128
+YOLO_batch_size = 16
 
 #############################
 ### Models Configurations ###
